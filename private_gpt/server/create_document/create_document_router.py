440b2cb0a4c0833d9202681e95ada38ef8f26e1e
Pabasara Jayawardhana <pabasarajayawardhana@gmail.com>
2023-12-05 23:18:15 -0500

    separete functions and iterate

--- private_gpt/server/create_document/create_document_router.py
+++ private_gpt/server/create_document/create_document_router.py
@@ -1,61 +1,65 @@
 from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import JSONResponse
-#from private_gpt.open_ai.openai_models import OpenAICompletion
-from private_gpt.open_ai.openai_models import (
-    OpenAICompletion,
-    OpenAIMessage,
-)
-from private_gpt.server.completions.completions_router import completions_router, prompt_completion, CompletionsBody
-from private_gpt.server.chat.chat_router import chat_router, chat_completion, ChatBody
 from private_gpt.server.chat.chat_service import ChatService
-
 from llama_index.llms import ChatMessage, ChatResponse, MessageRole
 
 create_document_router = APIRouter(prefix="/v1")
 
 
+def get_chat_responses(service: ChatService, messages: list[ChatMessage]) -> str:
 
-@create_document_router.post("/prompt-responses", response_model=list[dict])
-async def get_prompt_responses(request: Request) -> JSONResponse:
-    prompts = [
-        "What is the capital of France?",
-        "Who wrote Hamlet?",
-        "How does photosynthesis work?",
-    ]
+    #get chat responses from the chat service.
 
-    service = request.state.injector.get(ChatService)
-    all_messages = []
-    new_message = ChatMessage(content=prompts[0], role=MessageRole.USER)
-    all_messages.append(new_message)
+    full_response = ""
+    query_stream = service.stream_chat(messages=messages, use_context=True)
 
-    prompt_responses = []
-
-    query_stream = service.stream_chat(
-        messages=all_messages,
-        use_context=True,
-    )
-
-    full_response: str = ""
-    stream = query_stream.response
-    for delta in stream:
+    for delta in query_stream.response:
         if isinstance(delta, str):
             full_response += str(delta)
         elif isinstance(delta, ChatResponse):
             full_response += delta.delta or ""
 
-    prompt_responses = {"Test": full_response}
+    return full_response
 
-    # try:
-    #     for prompt in prompts:
-            # Step 1: Get completion from /completions route
-            
-            #completions_body = CompletionsBody(prompt=prompt)
-            #completion_response = prompt_completion(request=request, body=completions_body)
-            
 
-            #prompt_responses.append({"prompt": prompt, "response": chat_response})
-            
-    # except HTTPException as e:
-    #     return JSONResponse(content={"error": f"Error: {str(e)}"}, status_code=500)
+def process_prompts(service: ChatService, prompts: list[str]) -> dict:
 
-    return JSONResponse(content=prompt_responses)
+    #Process a list of prompts and get responses for each prompt.
+    
+    prompt_responses = {}
+
+    for idx, prompt in enumerate(prompts):
+        all_messages = [ChatMessage(content=prompt, role=MessageRole.USER)]
+        response = get_chat_responses(service, all_messages)
+        prompt_responses[f"Prompt_{idx + 1}"] = response
+
+    return prompt_responses
+
+
+@create_document_router.post("/prompt-responses", response_model=list[dict])
+async def get_prompt_responses(request: Request) -> JSONResponse:
+    prompts = [
+        """
+        Write a technical, paragraph OF MAXIMUM 350 WORD that
+        1. introduces the objective of the project the team worked on. Why they worked on it. What were they trying to achieve.
+        2. What were the standard process((es) or technology that is currently used to achieve the same result and why were these standard technologies insufficient.
+        3. End your paragraph with a statement in the format of: In order to develop a (state the objective of the system that they were trying to develop), we encountered the following technological uncertainties:
+        Now list in short bullet points the uncertainties that they encountered and one or two sentences about why the uncertainties exist and the difficulties they present towards achieving the objective or relevant sections of the objective of the project. Start each uncertainty statement with: We were uncertain whether we could develop a system that does (mention what the system is supposed to do with respect to this particular uncertainty and the goal of the project). Write a sentence about what the standard technology/technologies is to address this uncertainty and why it is insufficient.
+        Keep the tone academic, technical and report writing format, highlighting the core technology. Use the first person voice and assume you are the team.
+        """,
+        """
+        write an account of the work done, use an academic tone with MAXIMUM 700 WORDs, with a focus on highlighting the technology
+        break the work done down into a minimal number of paragraphs, where each paragraph demonstrates this cycle of work: hypotheses -> experimentation -> result
+        """,
+        """ 
+        The team achieved the following technological advancements:
+        The team gained new knowledge around the development of <paraphrase of the first TU sentence>. To this end, they learned that <paraphrase of the primary accomplishment(s) that map(s) to the TU’s hypothesis>. These technological advancements made it possible to <succinctly describe what the primary TA was>. Therefore, their hypothesis was proven (in)correct and the technological uncertainty was/remains (un)solved. Prior to the work performed by the team within the year, the standard practice was to <paraphrase standard practice sentence of the TU>.
+        """,
+    ]
+
+    try:
+        service = request.state.injector.get(ChatService)
+        prompt_responses = process_prompts(service, prompts)
+        return JSONResponse(content=prompt_responses)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
